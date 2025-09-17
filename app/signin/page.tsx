788851d90740ae059b78e4ff5df114705c234f57
SamKTA <150455266+SamKTA@users.noreply.github.com>
2025-09-17 16:17:52 +0200

    Create page.tsx

---
+++ app/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function SignInTest() {
+  return (
+    <main style={{ maxWidth: 360, margin: '64px auto' }}>
+      <h1>Page Signin (test)</h1>
+      <p>Si tu vois ceci, la route /signin existe.</p>
+    </main>
+  );
+}
