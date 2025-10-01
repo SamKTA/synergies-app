64091f644c955a5530427042044c154170de7b08
SamKTA <150455266+SamKTA@users.noreply.github.com>
2025-10-01 16:20:42 +0200

    Update tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{ts,tsx,js,jsx}',
+    './components/**/*.{ts,tsx,js,jsx}',
   ],
   theme: {
     extend: {},
